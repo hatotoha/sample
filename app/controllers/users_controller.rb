f589cd20454b144d3e13959449070814485a246c
hatotoha <26476905+hatotoha@users.noreply.github.com>
2018-12-01 16:40:43 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+  def show
+    # params[:id]は文字列型の "1" ですが、findメソッドでは自動的に整数型に変換されます
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root 'static_pages#home'
+  # 次は7.4から
 
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
 
   get '/signup', to: 'users#new' # get '/new', to: 'users#new', as: 'signup'
+  post '/signup',  to: 'users#create'
+
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
